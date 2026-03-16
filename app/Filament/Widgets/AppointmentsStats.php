81ec9200df0d9ab92a91bf56eab5224ddb7fa5d3
SayedERageh <said.rageh66@gmail.com>
2026-03-16 22:18:09 +0200

    new edit

--- app/Filament/Widgets/AppointmentsStats.php
+++ app/Filament/Widgets/AppointmentsStats.php
@@ -12,23 +12,28 @@ class AppointmentsStats extends StatsOverviewWidget
     protected function getStats(): array
     {
         return [
- Stat::make(
-                'إجمالي الكاش',
-                Appointment::whereDate('appointment_date', Carbon::today())
-                    ->where('payment_method', 'cash')
-                    ->selectRaw('SUM(paid + approval_difference) as total')
-                    ->value('total')
-            )
-            ->color('primary'),
 
             Stat::make(
-                'إجمالي الفيزا',
+                'المدفوع اليوم خ',
                 Appointment::whereDate('appointment_date', Carbon::today())
-                    ->where('payment_method', 'visa')
-                    ->selectRaw('SUM(paid + approval_difference) as total')
-                    ->value('total')
+                    ->sum('paid')
             )
-            ->color('info'),
+            ->color('success'),
+
+            Stat::make(
+                'إجمالي فرق الموافقة',
+                Appointment::whereDate('appointment_date', Carbon::today())
+                    ->sum('approval_difference')
+            )
+            ->color('warning'),
+
+       Stat::make(
+    'إجمالي دخل اليوم',
+    Appointment::whereDate('appointment_date', Carbon::today())
+        ->selectRaw('SUM(paid + IFNULL(approval_difference,0)) as total')
+        ->value('total')
+)
+->color('success'),
 
         ];
     }
